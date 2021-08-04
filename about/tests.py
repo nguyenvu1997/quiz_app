5d4862286d14a30ab8a0a12316f87db2db84ae16
nguyenvu1997 <nguyenanhvu19971997@gmail.com>
2021-08-04 23:39:18 +0700

    Add Test todolist about contact

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class AboutPageTests(TestCase):
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+class ContactPageTests(TestCase):
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class TaskListTests(TestCase):
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('tasks'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('tasks'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'task/task_list.html')
+
+class TaskCreateTests(TestCase):
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('task_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('task_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'task/task_create.html')
